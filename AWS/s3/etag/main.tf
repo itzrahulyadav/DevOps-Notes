18fc5bcebdf15a8183d9141a22520aa85546f089
Rahul Yadav <rahulkyadav9691@gmail.com>
2025-01-13 17:12:23 +0000

    add etag

--- AWS/s3/etag/main.tf
+++ AWS/s3/etag/main.tf
@@ -1,1 +1,24 @@
-main.tf file
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.83.1"
+    }
+  }
+}
+
+provider "aws" {
+   region = "ap-south-1"
+} 
+
+
+resource "aws_s3_bucket" "default" {
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.default.id
+  key    = "myfile.txt"
+  source = "myfile.txt"
+
+  etag = filemd5("myfile.txt")
+}
