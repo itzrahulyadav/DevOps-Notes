7f6ac31fc8b9231a22b20957a6a2ac2deb9e98d6
Rahul Kumar Yadav <65400893+itzrahulyadav@users.noreply.github.com>
2025-05-23 01:05:34 +0530

    Create install.sh

---
+++ AWS/ec2/vscode/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/vscodium-archive-keyring.gpg >/dev/null
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' | sudo tee /etc/apt/sources.list.d/vscodium.list
+sudo apt update && sudo apt install codium
+
+
+
+curl -fsSL https://code-server.dev/install.sh | sh
+code-server --auth none --bind-addr 0.0.0.0:8080
+
